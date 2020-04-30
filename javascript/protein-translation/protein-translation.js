eb399bcea39121737171b39df6d036ccc88392ca
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-30 12:31:10 -0400

    Refactor exercise

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,50 +1,47 @@
 export const translate = (rnaString = '') => {
-  if (rnaString.length === 0) { return [] }
-  const rnaStringLength = rnaString.length; 
-  const totCodons = rnaStringLength / 3;
   const proteinArr = [];
-
+  const rnaStringLength = rnaString.length; 
+  const totNumCodons = rnaStringLength / 3;
+  
+  if (rnaString.length === 0) { return proteinArr }
   if (rnaStringLength % 3 !== 0) { throw 'Invalid codon' };
 
-  for (let i = 0; i < totCodons; i++) {
+  for (let i = 0; i < totNumCodons; i++) {
     const index = 3 * i;
-
     const codon = rnaString.slice(index, index + 3);
 
-    if (codon === 'UAA' || codon === 'UAG' || codon === 'UGA') { return proteinArr }
+    if (codon === 'UAA' || codon === 'UAG' || codon === 'UGA') { return proteinArr };
 
-    switch (codon) {
-      case 'AUG':
-        proteinArr.push('Methionine');
-        break;
-      case 'UUU':
-      case 'UUC':
-        proteinArr.push('Phenylalanine');
-        break;
-      case 'UUA':
-      case 'UUG':
-        proteinArr.push('Leucine');
-        break;
-      case 'UCU':
-      case 'UCC':
-      case 'UCA':
-      case 'UCG':
-        proteinArr.push('Serine');
-        break;
-      case 'UAU':
-      case 'UAC':
-        proteinArr.push('Tyrosine');
-        break;
-      case 'UGU':
-      case 'UGC':
-        proteinArr.push('Cysteine');
-        break;
-      case 'UGG':
-        proteinArr.push('Tryptophan');
-        break;
-      default:
-        throw 'Invalid codon';
-    }
-  }
-  return proteinArr;
+    const proteinName = convertToProtein(codon);
+    proteinArr.push(proteinName);
   };
+  return proteinArr;
+};
+
+function convertToProtein(codon) {
+  switch (codon) {
+    case 'AUG':
+      return 'Methionine';
+    case 'UUU':
+    case 'UUC':
+      return 'Phenylalanine';
+    case 'UUA':
+    case 'UUG':
+      return 'Leucine';
+    case 'UCU':
+    case 'UCC':
+    case 'UCA':
+    case 'UCG':
+      return 'Serine';
+    case 'UAU':
+    case 'UAC':
+      return 'Tyrosine';
+    case 'UGU':
+    case 'UGC':
+      return 'Cysteine';
+    case 'UGG':
+      return 'Tryptophan';
+    default:
+      throw 'Invalid codon';
+  }
+}
