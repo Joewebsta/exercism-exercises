325ab994fd85318ab83c75146fcbff9f5c5347a0
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-30 11:51:39 -0400

    Complete protein-translation exercise and pass all tests

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,10 +1,50 @@
-export const translate = (rnaString) => {
-  const totCodons = rnaString.length / 3;
-  
+export const translate = (rnaString = '') => {
+  if (rnaString.length === 0) { return [] }
+  const rnaStringLength = rnaString.length; 
+  const totCodons = rnaStringLength / 3;
+  const proteinArr = [];
+
+  if (rnaStringLength % 3 !== 0) { throw 'Invalid codon' };
+
   for (let i = 0; i < totCodons; i++) {
     const index = 3 * i;
-    let codon = rnaString.slice(index, index + 3); 
-  }
-};
 
-translate('AUGUUUUCU');
+    const codon = rnaString.slice(index, index + 3);
+
+    if (codon === 'UAA' || codon === 'UAG' || codon === 'UGA') { return proteinArr }
+
+    switch (codon) {
+      case 'AUG':
+        proteinArr.push('Methionine');
+        break;
+      case 'UUU':
+      case 'UUC':
+        proteinArr.push('Phenylalanine');
+        break;
+      case 'UUA':
+      case 'UUG':
+        proteinArr.push('Leucine');
+        break;
+      case 'UCU':
+      case 'UCC':
+      case 'UCA':
+      case 'UCG':
+        proteinArr.push('Serine');
+        break;
+      case 'UAU':
+      case 'UAC':
+        proteinArr.push('Tyrosine');
+        break;
+      case 'UGU':
+      case 'UGC':
+        proteinArr.push('Cysteine');
+        break;
+      case 'UGG':
+        proteinArr.push('Tryptophan');
+        break;
+      default:
+        throw 'Invalid codon';
+    }
+  }
+  return proteinArr;
+  };
