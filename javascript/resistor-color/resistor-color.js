16874a7f4ec59043165eb8f78a0d3215525bfaa8
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-11 12:45:58 -0400

    Remove unecessary if statement

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,7 +1,5 @@
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-export const colorCode = (color = COLORS) => {
-  if (color === 'Colors') { return COLORS; }
-
+export const colorCode = (color) => {
   return COLORS.indexOf(color);
 };
