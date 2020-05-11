04a9cf0bdc52a28146aa4fe0045da862c61d781e
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-11 14:26:56 -0400

    Refactor resistor-color-duo exercise

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,5 @@
 const resistorColors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
 
 export const decodedValue = (colors) => {
-  const color1String = String(resistorColors.indexOf(colors[0]));
-  const color2String = String(resistorColors.indexOf(colors[1]));
-  return parseInt(color1String + color2String);
+  return parseInt("" + resistorColors.indexOf(colors[0]) + resistorColors.indexOf(colors[1]));
 };
