285f32d3d1e539a425f0bd5014e6fc690dd124d4
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-19 10:53:33 -0400

    Deleted completed files that were restored when branches were merged

--- javascript/completed/Side Exercises/02-armstrong-numbers/armstrong-numbers.js
+++
@@ -1,9 +0,0 @@
-export const isArmstrongNumber = (num) => {
-  const numArr = String(num).split('');
-  
-  const armstrongNum = numArr.reduce((sum, current) => {
-    return sum + Number(current) ** numArr.length;
-  }, 0)
-  
-  return (num === armstrongNum);
-  };
