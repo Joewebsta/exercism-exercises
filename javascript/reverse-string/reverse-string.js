e3cb0fa03b3cb9c6f5531c71b8a81b85fe0145af
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-20 11:17:30 -0400

    Refactor reverse-string exercise

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,11 +1,1 @@
-export const reverseString = (str) => {
-  const reverseStr = [];
-  const strLength = str.length;
-  const strLetters = str.split('');
-
-  for (let i = strLength - 1; i >= 0; i--) {
-    reverseStr.push(strLetters[i]);
-  }
-
-  return reverseStr.join('');
-};
+export const reverseString = str => [...str].reverse().join('');
