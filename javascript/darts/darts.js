efb8270b9b4172628c90596a1ddea92be9b0b2b5
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-01 15:22:44 -0400

    Complete dart exercise

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,8 +1,22 @@
-//
-// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+function isOnInner(x, y) {
+  return (x ** 2) + (y ** 2) <= 1; 
+}
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+function isOnMiddle(x, y) {
+  return (x ** 2) + (y ** 2) > 1 && (x ** 2) + (y ** 2) <= 25;
+}
+
+function isOnOuter(x, y) {
+  return (x ** 2) + (y ** 2) > 25 && (x ** 2) + (y ** 2) <= 100;
+}
+
+export const score = (x, y) => {
+  x = Math.abs(x);
+  y = Math.abs(y);
+
+  if (isOnInner(x, y))  { return 10; }
+  if (isOnMiddle(x, y)) { return 5; }
+  if (isOnOuter(x, y))  { return 1; }
+
+  return 0;  
 };
